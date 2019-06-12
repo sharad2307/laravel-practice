3a0b5edb9109d4e5b3308d53b1b132cea5e8a727
sharad2307 <manuandmehrotra@gmail.com>
2019-06-12 09:44:07 +0530

    form-updation

--- blog/app/Http/Controllers/ProjectsController.php
+++ blog/app/Http/Controllers/ProjectsController.php
@@ -20,6 +20,37 @@ class ProjectsController extends Controller
     	return view('projects.create');
     }
 
+
+     public function edit($id)
+    {
+    	$project = Project::find($id);
+    	return view('projects.edit', compact('project'));
+    }
+
+
+     public function show()
+    {
+    	
+    	return view('projects.show');
+    }
+
+
+     public function update($id)
+    {
+    	$project = Project::find($id);
+
+    	$project ->title = request('title');
+    	$project->description = request('description');
+    	$project->save();
+    	return redirect('/projects');
+    }
+
+     public function destroy()
+    {
+    	
+    	return view('projects.destroy');
+    }
+
      public function store()
     {
     	$project = new Project();
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -15,10 +15,27 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/projects','ProjectsController@index');
 
-Route::post('/projects','ProjectsController@store');
+Route::resource('projects','ProjectsController');
 
 
-Route::get('/projects/create','ProjectsController@create');
+
+// Route::get('/projects','ProjectsController@index');
+
+// Route::get('/projects/create','ProjectsController@create');
+
+// Route::get('/projects/{project}','ProjectsController@show');
+
+
+// Route::post('/projects','ProjectsController@store');
+
+// Route::get('/projects/{project}/edit','ProjectsController@edit');
+
+// Route::patch('/projects/{project}','ProjectsController@update');
+
+// Route::delete('/projects/{project}','ProjectsController@destroy');
+
+
+
+
 
